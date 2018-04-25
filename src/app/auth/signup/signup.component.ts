0ae94125a6cfbb9c787566a25d5c82bec68b0466
Charles Lennon <charleslcsantos@gmail.com>
2018-04-25 14:34:47 -0300

    adicionando mensagem de erro nas telas de auth

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -9,22 +9,40 @@ import { AuthService } from '../auth.service';
   styleUrls: ['./signup.component.scss']
 })
 export class SignupComponent implements OnInit {
+  error: string = '';
+  loading: boolean = false;
   public name: string;
   public email: string;
   public password: string;
   public password2: string;
 
-  constructor(private router: Router, private authService: AuthService) {}
+  constructor(
+    private router: Router,
+    private authService: AuthService
+  ) {}
 
   ngOnInit() {}
 
   signUp() {
+    this.loading = true;
     this.authService.signup(this.email, this.password).subscribe(
       user => {
         this.router.navigate(['']);
       },
       error => {
+        this.loading = false;
         console.log(error);
+        switch (error.code) {
+          case 'auth/email-already-in-use':
+            this.error = 'Ops! Este email já possui conta no meuPonto.';
+            break;
+          case 'auth/invalid-email':
+            this.error = 'Você precisa colocar um email válido. ex: email@email.com';
+            break;
+          default:
+            this.error = error.message;
+            break;
+        }
       }
     );
   }
